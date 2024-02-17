ae072aed498f9ca55733518b25edde333ea7e021
AlekseyYarina <aleksey.yarina@gmail.com>
2024-02-17 23:12:15 +0200

    fix: task 1 & task 2

--- src/2.ts
+++ src/2.ts
@@ -36,10 +36,12 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
-  constructor(salary: number) {
-    super("Max", "Management", salary + 10000);
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
   }
 }
+
+const manager = new Manager("Max", "Management", 20000);
+console.log(manager.getEmployeeDetails());
 
 export {};
